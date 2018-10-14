e56a52d7d64a1871b7fce6f3b2deffc0bcddc723
DxExWxExY <DxExWxExY@gmail.com>
2018-10-14 13:57:22 -0700

    Added Server Events

--- sft/client.py
+++ sft/client.py
@@ -1,35 +1,33 @@
 #! /usr/bin/env python3
 
 # Echo client program
-import socket, sys, re
+import socket, sys, re, os
 import params
 from framedSock import FramedStreamSock
 from threading import Thread
 from threading import Lock
 import time
 
-switchesVarDefaults = (
+def init_client():
+    global serverHost, serverPort, debug
+    switchesVarDefaults = (
     (('-s', '--server'), 'server', "localhost:50001"),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
     (('-?', '--usage'), "usage", False), # boolean (set if present)
     )
-
-
-progname = "framedClient"
-paramMap = params.parseParams(switchesVarDefaults)
-
-server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
-
-if usage:
-    params.usage()
-
-
-try:
-    serverHost, serverPort = re.split(":", server)
-    serverPort = int(serverPort)
-except:
-    print("Can't parse server:port from '%s'" % server)
-    sys.exit(1)
+    progname = "Client"
+    paramMap = params.parseParams(switchesVarDefaults)
+    
+    server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+    
+    if usage:
+        params.usage()
+    try:
+        serverHost, serverPort = re.split(":", server)
+        serverPort = int(serverPort)
+    except:
+        print("Can't parse server:port from '%s'" % server)
+        sys.exit(1)
 
 class ClientThread(Thread):
     def __init__(self, serverHost, serverPort, debug):
@@ -37,7 +35,6 @@ class ClientThread(Thread):
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
-        global lock
         s = None
         for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
             af, socktype, proto, canonname, sa = res
@@ -58,19 +55,44 @@ class ClientThread(Thread):
                 continue
             break   
         if s is None:
-           print('could not open socket')
-           sys.exit(1)
-        if lock.acquire():
-            fs = FramedStreamSock(s, debug=debug)
-            print("sending hello world")
-            fs.sendmsg(b"hello world")
-            print("received:", fs.receivemsg())
-            lock.release()
+            print('could not open socket')
+            sys.exit(1)
+        
+        fs = FramedStreamSock(s, debug=debug)
+        send_message(fs, "put t.txt")
+        
+def send_message(fs, *args):
+    user_input = args[0]
+    if re.match("put\s[\w\W]+", user_input):
+        trash, file = user_input.split(" ",1)
+        if os.path.exists("%s/%s" % (os.getcwd(), file)):
+            msg = "put " + file.rsplit('/', 1)[-1]
+            fs.sendmsg(msg.encode())
+            fs.sendmsg(open(file, "rb").read())
+        else:
+            print("\nFile Doesn't Exist.")
+    elif re.match("get\s[\w\W]+", user_input):
+        fs.sendmsg(user_input.encode())
+        payload = fs.receivemsg()
+        if payload.decode() == "true":
+            trash, file = user_input.split(" ",1)
+            writer = open("%s/%s" % (os.getcwd(), file), "wb+")
+            payload = fs.receivemsg()
+            writer.write(payload)
+            writer.close()
+            print("\nTransfer Done.")
+        else:
+            print("File not Found.")
+    elif user_input == "quit":
+        print("Killing Client...")
+        sys.exit(0)
+    else:
+        fs.sendmsg(args[0].encode())
+        print(fs.receivemsg())
+        fs.sendmsg(args[0].encode())
+        print(fs.receivemsg())
 
-
-lock = Lock()
-# event.clear()
-for i in range(100):
-
-    ClientThread(serverHost, serverPort, debug)
-
+if __name__ == '__main__':
+    init_client()
+    for i in range(1):
+        ClientThread(serverHost, serverPort, debug)
--- sft/server.py
+++ sft/server.py
@@ -1,28 +1,30 @@
 #! /usr/bin/env python3
-import sys, os, socket, params, time
+import sys, os, socket, params, time, re
 from threading import Thread
 from threading import Event
 from framedSock import FramedStreamSock
 
-switchesVarDefaults = (
-    (('-l', '--listenPort') ,'listenPort', 50001),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
+def init_server():
+    global sock, debug, lsock
+    switchesVarDefaults = (
+        (('-l', '--listenPort') ,'listenPort', 50001),
+        (('-d', '--debug'), "debug", False), # boolean (set if present)
+        (('-?', '--usage'), "usage", False), # boolean (set if present)
+        )
 
-progname = "echoserver"
-paramMap = params.parseParams(switchesVarDefaults)
+    progname = "echoserver"
+    paramMap = params.parseParams(switchesVarDefaults)
 
-debug, listenPort = paramMap['debug'], paramMap['listenPort']
+    debug, listenPort = paramMap['debug'], paramMap['listenPort']
 
-if paramMap['usage']:
-    params.usage()
+    if paramMap['usage']:
+        params.usage()
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
-bindAddr = ("127.0.0.1", listenPort)
-lsock.bind(bindAddr)
-lsock.listen(5)
-print("listening on:", bindAddr)
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+    bindAddr = ("127.0.0.1", listenPort)
+    lsock.bind(bindAddr)
+    lsock.listen(5)
+    print("listening on:", bindAddr)
 
 class ServerThread(Thread):
     requestCount = 0            # one instance / class
@@ -33,22 +35,44 @@ class ServerThread(Thread):
     def run(self):
         global event
         while True:
-            # if not event.is_set():
-            #     event.set()
-            msg = self.fsock.receivemsg()
-            if not msg:
-                if self.debug: print(self.fsock, "server thread done")
-                return
-            requestNum = ServerThread.requestCount
-            time.sleep(0.001)
-            ServerThread.requestCount = requestNum + 1
-            msg = ("%s! (%d)" % (msg, requestNum)).encode()
-            print(msg)
-            self.fsock.sendmsg(msg)
-                # event.clear()
-            
+            if event.wait():
+                event.clear()
+                server_protocols(self.fsock)
+                event.set()
 
-event = Event()
-while True:
-    sock, addr = lsock.accept()
-    ServerThread(sock, debug)
+def server_protocols(fsock):
+    payload = fsock.receivemsg()
+    if payload:
+        print("Received: ", payload.decode())
+        if re.match("put\s[\w\W]+", payload.decode()):
+            trash, file = payload.decode().split(" ",1)
+            if not os.path.exists("%s/__pycache__/%s" % (os.getcwd(), file)):
+                writer = open("%s/__pycache__/%s" % (os.getcwd(), file), "wb+")
+                fsock.sendmsg("start".encode())
+                payload = fsock.receivemsg()
+                writer.write(payload)
+                writer.close()
+                print("Transfer Done.")
+            else:
+               fsock.sendmsg("File Already Exists.".encode()) 
+        elif re.match("get\s[\w\W]+", payload.decode()):
+            trash, file = payload.decode().split(" ",1)
+            if os.path.exists("%s/__pycache__/%s" % (os.getcwd(),file)):
+                fsock.sendmsg("true".encode())
+                fsock.sendmsg(open("%s/__pycache__/%s" % (os.getcwd(),file), "rb").read(), 1)
+                print("Transfer Done.")
+            else:
+                fsock.sendmsg("File Doesn't Exist.".encode())
+        else:
+            fsock.sendmsg("Received".encode())
+    else:
+        event.set()
+        return
+            
+if __name__ == '__main__':
+    init_server()
+    event = Event()
+    event.set()
+    while True:
+        sock, addr = lsock.accept()
+        ServerThread(sock, debug)
