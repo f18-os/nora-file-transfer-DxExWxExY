6824c9d94cf0b96d09b9c05ca61d4525685e4d2b
DxExWxExY <DxExWxExY@gmail.com>
2018-10-13 21:27:22 -0700

    Thread Locking in Client

---
+++ sft/server.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+import sys, os, socket, params, time
+from threading import Thread
+from threading import Event
+from framedSock import FramedStreamSock
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
+
+class ServerThread(Thread):
+    requestCount = 0            # one instance / class
+    def __init__(self, sock, debug):
+        Thread.__init__(self, daemon=True)
+        self.fsock, self.debug = FramedStreamSock(sock, debug), debug
+        self.start()
+    def run(self):
+        global event
+        while True:
+            # if not event.is_set():
+            #     event.set()
+            msg = self.fsock.receivemsg()
+            if not msg:
+                if self.debug: print(self.fsock, "server thread done")
+                return
+            requestNum = ServerThread.requestCount
+            time.sleep(0.001)
+            ServerThread.requestCount = requestNum + 1
+            msg = ("%s! (%d)" % (msg, requestNum)).encode()
+            print(msg)
+            self.fsock.sendmsg(msg)
+                # event.clear()
+            
+
+event = Event()
+while True:
+    sock, addr = lsock.accept()
+    ServerThread(sock, debug)
